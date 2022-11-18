d3da0605a6147b9f7c3e064da107696e672e61fe
Helen Lehrer <hlehrer@outlook.com>
2022-11-18 11:08:58 -0800

    Add NavBar component

--- src/components/InventoryControl.js
+++ src/components/InventoryControl.js
@@ -11,7 +11,7 @@ class InventoryControl extends React.Component {
       formVisibleOnPage: false,
       mainInventoryList: [],
       selectedInventory: null,
-      editing: false
+      // editing: false
     };
   }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar(){
+  return (
+  <React.Fragment>
+    <nav>
+      <h1>The Tao of Tea Inventory Tracker</h1>
+    </nav>
+  </React.Fragment>
+  )
+}
+
+  export default NavBar;
