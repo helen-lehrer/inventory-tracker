afed9211c3ae9c954064026779964f7227fd17c4
Helen Lehrer <hlehrer@outlook.com>
2022-11-22 16:23:10 -0800

    add css styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import tea from "./../img/tea.png";
+import './../index.css';
 
 function NavBar(){
+  const style = {
+    all: {
+    backgroundColor: '#4e7a05',
+    fontFamily: 'Amatic SC',
+    paddingTop: '20',
+    }
+  }
   return (
   <React.Fragment>
+      <div style={style.all}> 
     <nav>
       <h1>The Tao of Tea Inventory Tracker</h1>
     </nav>
+    </div>
+    <img src={tea} style={style.img} width="100px"/>
   </React.Fragment>
   )
 }
